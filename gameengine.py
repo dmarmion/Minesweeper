13a2301437e5f97d8707295bf7c2d28683e99812
greenmallow <dmarmion08@gmail.com>
2021-06-11 23:40:41 +1000

    Add basic game engine

--- gameengine.py
+++ gameengine.py
@@ -1,2 +1,20 @@
+import time
+
+from models import Grid
+
 class GameEngine:
     """Model representing a game of Minesweeper"""
+
+    def __init__(self):
+        # Set up a game
+        self._grid = Grid()
+        self.game_over = True
+        self.game_start_time = time.time()
+    
+    def uncover_cell(self, row, col):
+        """Uncover a Cell in the grid."""
+        pass
+
+    def flag_cell(self, row, col):
+        """Flag a cell in the grid as containing a mine."""
+        pass
